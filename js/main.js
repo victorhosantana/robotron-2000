931c67aa293b041d6b77e33ed8b6b1739419d647
Victor Santana <103072762+victorhosantana@users.noreply.github.com>
2023-06-19 20:17:08 +0100

    implementacoes no arquivo main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,69 @@
-const robotron = document.getElementById('robotron')
+// Variável com todos os botões de equipamentos
+const botaoAjusteEquipamentos = document.querySelectorAll('[data-controle]');
+const estatisticas = document.querySelectorAll('[data-estatistica]')
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-robotron.addEventListener('click', alertaInicialNoConsole);
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
-function alertaInicialNoConsole () {
-    console.log('Olá, mundo!');
+// Identificação do clique na página
+botaoAjusteEquipamentos.forEach( (botaoAjuste) => {
+    botaoAjuste.addEventListener('click', (evento) => {
+        ajustaValorEquipamento(evento.target.parentElement, evento.target.dataset.controle);
+        atualizaEstatisticas(evento.target.dataset.peca, evento.target.textContent);
+    });
+});
+
+// Evento de alteração dos valores
+function ajustaValorEquipamento (equipamento, ajuste) {
+    const peca = equipamento.querySelector('[data-contador]'); // Definição da peça correta
+
+    if (ajuste === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else if (ajuste === "+") {
+        peca.value = parseInt(peca.value) + 1;
+    } else {
+        console.log("Nenhuma opção identificada.");
+    }
+}
+
+// Evento de reclaculo das Estatísticas
+function atualizaEstatisticas (peca, sentidoAjuste) {
+    estatisticas.forEach( (estatisticaUnica) => {
+        if (sentidoAjuste === '+') {
+            estatisticaUnica.textContent = parseInt(estatisticaUnica.textContent) + pecas[peca][estatisticaUnica.dataset.estatistica];
+        }
+        else if (sentidoAjuste === '-') {
+            estatisticaUnica.textContent = parseInt(estatisticaUnica.textContent) - pecas[peca][estatisticaUnica.dataset.estatistica];
+        }
+    });
 }
